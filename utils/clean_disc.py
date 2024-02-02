6bd4cd8fadb7807cef21b31fece4e45382efc447
mehni <lozomehni28@gmail.com>
2024-02-02 12:34:24 +0100

    add unkown update

--- utils/clean_disc.py
+++ utils/clean_disc.py
@@ -9,9 +9,9 @@ def clean_sents_save(sents, name):
     dump(sents,open(name,'wb'))
     print("------------ SAVING %s ------------" % name)
 
-def freq(contents): # lines contents for frequency table creation
+def freq(cts): # lines contents for frequency table creation
     v = Counter()
-    for l in contents:
+    for l in cts:
         tks = l.split()
         v.update(tks)
     return v
@@ -20,3 +20,17 @@ def trimming(vocab, min_occ):
     # reducing table size from all non frequent words
     tks = [k for k, o in vocab.items() if o >= min_occ]
     return set(tks)
+
+def update(cts,vocab): ## updating lines containing OOV -> UNK
+    new_ls = list()  
+    for l in cts:
+        new_tks = list()
+        for tk in l.split():
+            if tk in vocab:
+                new_tks.append(tk)
+            else:
+                new_tks.append('unk')
+        new_l = ' '.join(new_tks)
+        new_ls.append(new_l)
+    return new_ls
+    
